6c8c4ee4213267bb947017ab6928e8e69dc53e32
Emanuel Maia <tavanomanu@gmail.com>
2024-02-11 00:52:16 +0000

    Add checks for upgrades list.

--- javascript/main.js
+++ javascript/main.js
@@ -25,9 +25,11 @@ let store_multiplier = 1; // not saving it on local storage for now (well, techn
 let store_buy = true;
 let store_sell_all = false;
 
+// makes sure everything is correct and up to date
 checkHelperList();
 checkUpgradeList();
 
+// visuals
 displayScore();
 displaySPS();
 generateHelperList();
@@ -50,134 +52,6 @@ setInterval(function() {
 setInterval(function() {
   updateTotalFarmed();
 }, 1000);
-
-// for updating the helper list when new helpers are added or something is modified
-// THIS **ISN'T** AS BAD AS IT LOOKS, I PROMISE, OK?
-function checkHelperList() {
-  // situations:
-  // -1 - calculate the total farmed from the total cost of onwned buildings
-  // 0 - buildings are removed (list is smaller)
-  // 1 - new buildings are added
-  // 2 - building characteristics are changed
-  // 3 - base cost is changed: requires recalculating next buy cost
-
-  // part -1
-  // this isn't really precise or pretty but it does do the trick so... can't really get any better than this
-  // unless you have that crystal ball that hiruzen had in the beginning of naruto that was never shown again
-  // in the series for some reason #borutoisntcanon
-  if (!game_total_farmed || game_total_farmed < 0) {
-    let estimate = score != undefined ? score : 0;
-    for (const helper of helpers) {
-      estimate += helper.base_cost * (Math.pow(1.15, helper.quantity) - Math.pow(1.15, 0)) / (1.15 - 1);
-    } 
-    game_total_farmed = estimate;
-  }
-
-  // part 0
-  if (helpers.length > default_helper_list.length) {
-    helpers.splice(default_helper_list.length, helpers.length - default_helper_list.length);
-  }
-
-  // part 1
-  let new_len = default_helper_list.length;
-  let old_len = helpers.length;
-  
-  if (old_len < new_len) {
-    for (let i = old_len; i < new_len; i++) {
-      // to prevent my dumbassery of adding shit with the same name, which fucks up the buttons
-      if (!helpers.some(obj => obj["name"] === default_helper_list.slice(i, i + 1)[0].name)) {
-        helpers.push(default_helper_list.slice(i, i + 1)[0]);
-      }
-    }
-  }
-
-  // part 2
-  for (let i = 0; i < helpers.length && i < default_helper_list.length; i++) {
-    if (helpers[i].name !== default_helper_list[i].name) {
-      helpers[i].name = default_helper_list[i].name;
-    }
-
-    if (helpers[i].base_cost !== default_helper_list[i].base_cost) {
-      helpers[i].base_cost = default_helper_list[i].base_cost;
-      helpers[i].cost = helpers[i].base_cost * Math.pow(1.15, helpers[i].quantity);
-      console.log(`updated price of ${helpers[i].name}: ${helpers[i].cost}`)
-    }
-
-    if (helpers[i].base_sps !== default_helper_list[i].base_sps) {
-      calcTotalSPS(); // recalculates the current total sus per second
-    }
-
-    if (helpers[i].description !== default_helper_list[i].description) {
-      helpers[i].description = default_helper_list[i].description;
-    }
-
-    if (!Number.isInteger(helpers[i].quantity)) {
-      if (typeof Math.floor(+helpers[i].quantity) == "number" && Math.floor(+helpers[i].quantity) >= 0) {
-        helpers[i].quantity = Math.floor(+helpers[i].quantity);
-      } else {
-        helpers[i].quantity = 0;
-      }
-    } else if (helpers[i].quantity < 0) {
-      helpers[i].quantity = 0;
-    }
-
-    if (helpers[i].sfx_quantity !== default_helper_list[i].sfx_quantity) {
-      helpers[i].sfx_quantity = default_helper_list[i].sfx_quantity;
-    }
-
-    if (helpers[i].total_farmed == undefined) {
-      // this solution assumes that I've already implemented the calculation of total
-      // sus farmed for older version compatibility
-      helpers[i].total_farmed = (helpers[i].quantity * helpers[i].sps / sus_per_second) * game_total_farmed;
-    }
-
-    if (helpers[i].verb !== default_helper_list[i].verb) {
-      helpers[i].verb = default_helper_list[i].verb;
-    }
-
-    if (default_helper_list[i].icon_number !== undefined && helpers[i].icon_number !== default_helper_list[i].icon_number) {
-      helpers[i].icon_number = default_helper_list[i].icon_number;
-    }
-
-    if (default_helper_list[i].has_dynamic_content !== undefined && helpers[i].has_dynamic_content !== default_helper_list[i].has_dynamic_content) {
-      helpers[i].has_dynamic_content = default_helper_list[i].has_dynamic_content;
-    }
-
-    if (helpers[i].icon !== default_helper_list[i].icon) {
-      if (helpers[i].has_dynamic_content) {
-        helpers[i].icon = default_helper_list[i].icon; // the icon path might be different if it was changed, so we need to make sure the base is the same
-        helpers[i].icon_number = calculateHelperOwnedUpgrades(helpers[i]) + 1; // plus one since they technically begin at 1 (not technically, they really just begin at 1)
-        helpers[i].icon = determineImageNumber(helpers[i].icon, helpers[i].icon_number);
-        console.log(`owned upgrades for helper ${helpers[i].name}: ${calculateHelperOwnedUpgrades(helpers[i])}`) 
-        console.log(`as such, the determined image path for ${helpers[i].name} is ${helpers[i].icon}`)
-      } else {
-      	helpers[i].icon = default_helper_list[i].icon;
-      }
-    }
-
-    if (default_helper_list[i].sfx_number !== undefined && helpers[i].sfx_number !== default_helper_list[i].sfx_number) {
-      if (helpers[i].has_dynamic_content) {
-        helpers[i].sfx_number = calculateHelperOwnedUpgrades(helpers[i]) + 1;
-      } else {
-        helpers[i].sfx_number = default_helper_list[i].sfx_number;
-      }
-    }
-
-    if (helpers[i].sound_path !== default_helper_list[i].sound_path) {
-      helpers[i].sound_path = default_helper_list[i].sound_path;
-    }
-  }
-}
-
-// do same thing but for upgrades
-function checkUpgradeList() {
-  /* get the function references (they're not stored because of json) */
-  for (let i = 0; i < upgrades.length; i++) {
-    for (let j = 0; j < upgrades[i].length; j++) {
-      upgrades[i][j].action = default_upgrade_list[i][j].action;
-    }
-  }
-}
 
 
 /*
